ccd8bc2ff6d2afce5512cce38ac9523dbbf6e6c3
Amruth S K <amruthsk25@gmail.com>
2025-12-18 19:48:45 +0530

    feat(db): Setup Mongoose, connect to MongoDB Atlas, and implement environment-aware connection config

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const app = express();
 //Mounting api router
 const apiRouter = require("./routes");
 const errorHandler = require("./utils/errorHandler");
+const connectToDb = require("./config/db.config");
+const mongoose = require("mongoose");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true})); 
@@ -20,6 +22,18 @@ app.get ('/ping', (req, res) => {
 
 app.use(errorHandler);  
 
-app.listen(PORT, () => {
-    console.log(`Server started at port: ${PORT}`);
+app.listen(PORT, async() => {
+  console.log(`Server started at port: ${PORT}`);
+  await connectToDb();
+  console.log("Successfully connected to db");
+
+  //dummy code for testing
+  const kittySchema = new mongoose.Schema({
+    name: String,
+  });
+  const Kitten = mongoose.model("Kitten", kittySchema);
+
+  const silence = new Kitten({ name: "Silence" });
+  await silence.save();
+  console.log(silence.name); // 'Silence'
 });
